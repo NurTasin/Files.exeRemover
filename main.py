b9cf44e049e0a86fa4fda266f5a8049bb02c7fa9
Nur Mahmud Ul Alam Tasin <62534505+NurTasin@users.noreply.github.com>
2023-10-07 01:16:35 +0600

    Updated this code to be mature enough!
    Updated the scanning mechanism and removed the foolish loading loop at the start

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os
 import time
 import sys
 import random
-for i in range(101):
-	print("Loading engine: "+str(i)+" %",end="\r")
-	time.sleep(random.uniform(0.05,0.1))
 
 def timer(func):
 	def wrapper(*args,**kwargs):
@@ -14,43 +11,25 @@ def timer(func):
 		return var
 	return wrapper
 
-global checking_path
-
-global python_version
-
 python_version=int(sys.version[0])
-
-if python_version==2:
-	print("[Warning!!] You are running \"Files.exe remover\" on Python 2. This app will not be developed anymore using Python 2.\n[Sollution!!]Upgrade your Python to Python 3 for staying uppdated")
-	checking_path=str(raw_input("\nEnter the path to check: "))
-elif python_version==3:
-	checking_path=str(input("Enter the path to check: "))
+checking_path=str(input("Enter the path to check: "))
 
 @timer
 def scanFiles_exe(path):
 	virusFound=[]
 	for root, directory,files in os.walk(path):
 		for i in directory:
-			try:
-				file__=open("{}/{}/{}.exe".format(root,i,i),"r")
-				file__.close()
-				virusFound.append("{}/{}/{}.exe".format(root,i,i))
-			except NotADirectoryError:
-				pass
-			except FileNotFoundError:
-				pass
-			except FileExistsError:
-				pass
+			print(os.path.join(root,i))
+			if os.path.exists(os.path.join(root,i,f"{i} .exe")):
+				virusFound.append(os.path.join(root,i,f"{i} .exe"))
+
 	return virusFound
 
 def virus_found(lst):
 	for i in lst:
 		print("Virus found at {}".format(i))
-	choice=""
-	if python_version==2:
-		choice=raw_input("\n\nDo you want to delete them? [Y/n] ")
-	elif python_version==3:
-		choice=input("\n\nDo you want to delete them? [Y/n] ")
+	
+	choice=input("\n\nDo you want to delete them? [Y/n] ")
 	return choice.lower()
 
 def removeVirus(lst):
@@ -59,12 +38,12 @@ def removeVirus(lst):
 virus_list=scanFiles_exe(checking_path)
 if(len(virus_list)>0):
 	decision=virus_found(virus_list)
-if(decision=='y'):
-	removeVirus(virus_list)
-elif(decision=='n'):
-	print("Virus scanned but not deleted!!!")
-else:
-	print("Wrong choice made!! Terminating.....")
+	if(decision=='y'):
+		removeVirus(virus_list)
+	elif(decision=='n'):
+		print("Virus scanned but not deleted!!!")
+	else:
+		print("Wrong choice made!! Terminating.....")
 
 
 print("\n\nDeveloped by NurTasin. You can get latest update of this tool at https://github.com/NurTasin/Files.exeRemover.git")
